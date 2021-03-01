2a603baa1edf8b743c03420e86b84aafffc188af
Modern Viking <thomascustoms715@gmail.com>
2021-03-01 12:23:06 -0700

    basic blog site

---
+++ client/build/precache-manifest.ec85913a2cdbff291713380e34960037.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "faefb84387b3fabdcb8c",
+    "url": "/static/js/main.c0d18183.chunk.js"
+  },
+  {
+    "revision": "822f9bec589d06b36086",
+    "url": "/static/js/2.b4d67c0e.chunk.js"
+  },
+  {
+    "revision": "faefb84387b3fabdcb8c",
+    "url": "/static/css/main.3e6528b8.chunk.css"
+  },
+  {
+    "revision": "03426807854dc86b63cf8b33c955ea7e",
+    "url": "/index.html"
+  }
+];
